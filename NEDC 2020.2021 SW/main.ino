86b641d85fbc06a5957491b17491afd577711a9f
npg <npgoalkeeper@gmail.com>
2021-01-22 19:21:09 -0800

    Fix Arduino warnings

--- NEDC 2020.2021 SW/main.ino
+++ NEDC 2020.2021 SW/main.ino
@@ -23,6 +23,8 @@
 */
 
 // the setup function runs once when you press reset or power the board
+
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
