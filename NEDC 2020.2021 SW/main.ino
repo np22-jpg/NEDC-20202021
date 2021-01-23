5a5bb0cdc77b4d9682b7c9b3cd59e9a4685c8b5e
npg <npgoalkeeper@gmail.com>
2021-01-23 09:34:44 -0800

    Add initial deisgn proposals and arduino file hierarchy

--- NEDC 2020.2021 SW/main.ino
+++ NEDC 2020.2021 SW/main.ino
@@ -23,19 +23,19 @@
 */
 
 // the setup function runs once when you press reset or power the board
-
+#include "pins.h"
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
 }
 
 // This is just a placeholder to see if this repo actually works.
